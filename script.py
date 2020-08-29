8f7ed1a0f6b3b8932d1f0c67aa8ff3149b943577
117Coder <35137348+117Coder@users.noreply.github.com>
2020-08-29 16:51:36 +0100

    Added extra functionality
    Can now enter conversation mode, as the recogniser now recognises foreign speech as well as english.
    Added functionality to swap between conversation and single translation mode.
    Added second language dictionary for recognition (the old one was purely for translation).
    Added a tutorial.
    Added a beep sound effect to signify when microphone is open
    revised speak and translation functions by added extra parameters

--- script.py
+++ script.py
@@ -21,6 +21,9 @@ class bcolors:
 in_use = True
 srinp = ""
 thelang = ''
+thelang_keyVal = ''
+convo_mode = False
+oppoLang = ''
 languageDict = {"french":"fr",
                 "chinese":"zh-cn",
                 "srilankan":"si",
@@ -38,22 +41,57 @@ languageDict = {"french":"fr",
                 "greek":"el",
                 "italian":"it"}
 
+recieveLangDict = {"french":'fr-FR',
+                   "chinese":"zh-CN",
+                   "srilankan":"si-LK",
+                   "sri lankan":"si-LK",
+                   "russian":"ru-RU",
+                   "polish":"pl",
+                   "romanian":"ro-RO",
+                   "dutch":"dl-DL",
+                   "latin":"la",
+                   "japanese":"ja",
+                   "hindi":"hi-IN",
+                   "spanish":"es-ES",
+                   "german":"de-DE",
+                   "greek":"el-GR",
+                   "italian":"it-IT"}
+
+
+
+
+
+
+
+
+
 def initialise():
     global languageDict
     global thelang
     is_init = False
+    global oppoLang
+    global convo_mode
     speak("What language shall your voice be translated into", 'en-uk')
-    retrieve()
+    retrieve('en-GB')
     #print(srinp)
     for key in languageDict.keys():
         if srinp.lower() == key:
             is_init = True
             thelang = languageDict[key]
-            #print(thelang)
+            thelang_keyVal = key
+            oppoLang = recieveLangDict[key]
+            print(thelang + "   " + oppoLang)
     if is_init == False:
         speak("Please say a valid Language. Try Again", 'en-uk')
         initialise()
     else:
+        speak("Okay, one last step. Would you like to enter conversation mode?", 'en-uk')
+        retrieve('en-GB')
+        if srinp.lower() == "yes" or srinp.lower == "okay" or srinp.lower == "sure":
+            convo_mode = True
+        else:
+            speak("Okay then, single translation it is!", 'en-uk')
+            convo_mode = False
         speak("Okay, Setup complete, the translator is ready", 'en-uk')
         print("")
         print("")
@@ -62,15 +100,28 @@ def initialise():
         print("")
             
 
-def retrieve():
+
+
+
+
+
+
+
+
+def retrieve(langRET):
     global srinp
     global in_use
+    
     r = sr.Recognizer()                                                                                   
     with sr.Microphone() as source:                                                                       
-        print(bcolors.OKBLUE + "MICROPHONE ON:" + bcolors.ENDC + "Please Speak")                                                                                   
+        print(bcolors.OKBLUE + "MICROPHONE ON:" + bcolors.ENDC + "Please Speak")
+        playsound("beep.mp3")
         audio = r.listen(source) 
     try:
-        text = r.recognize_google(audio)   
+        #print(langRET)
+        text = r.recognize_google(audio, language=langRET)
+        #print("x " +r.recognize_google(audio, language='fr-FR') + " y")
+        
         print(bcolors.WARNING + "You said :" + bcolors.ENDC +" {}".format(text))
         if text == "setup" or text == "set up":
             print("-------SETUP-------")
@@ -82,41 +133,125 @@ def retrieve():
             in_use = False
         srinp = text
         
-    except:
-        print("ERROR")
+    except Exception as e:
+        print("Could not make that out, please say it louder")
+        speak("Could not make that out, please say it louder", 'en')
+        print(e)
+
+
+
+
+
+
+
+
         
-def translate_to(inp,lang):
+def translate_to(inp,lang,langIN):
     global thetranslation
     text = inp
     translator = Translator()
-    translatedText = translator.translate(inp,dest=lang)
+    translatedText = translator.translate(inp,dest=lang,src=langIN)
     print(bcolors.HEADER + "----TRANSLATION----" + bcolors.ENDC)
     print("TRANSLATION = " + translatedText.text)
     speak(translatedText.text,lang)
 
-def speak(temp,lang):
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+def speak(temp, lang):
     voice = gTTS(text=temp, lang=lang)
-    voice.save("testsound.mp3")
+    voice.save("voice.mp3")
     print(bcolors.FAIL + "Speaking," + bcolors.ENDC + bcolors.OKGREEN + "DO NOT SPEAK" + bcolors.ENDC)
-    playsound("testsound.mp3")
+    playsound("voice.mp3")
     time.sleep(1)
-    os.remove("testsound.mp3")
+    os.remove("voice.mp3")
+
+
+
+
+
+
+
+def prereq():
+    skip = False
+    speak("Hello, welcome to PyTranslator. Do you want to skip the tutorial?", "en")
+    retrieve('en-gb')
+    if srinp.lower() == "yes" or srinp.lower() == "okay" or  srinp.lower() == "sure" or  srinp.lower() == "please":
+        skip = True
+        initialise()
+    else:
+        speak("Okay, welcome to PyTranslator", "en")
+        speak("This is a tool for translation, you can translate in a conversation, between english and another language, or single translation", "en")
+        speak("To use this tool properly, remember, only speak after you hear a beep, that means your microphone is ready", "en")
+        speak("If you are in conversation mode, the microphone alternates between english and your other language", "en")
+        speak("Supported languages include:", "en")
+        speak("Frech, Chinese, Sri Lankan, Tamil, Russian, Polish, Romanian, Dutch, Latin, Japanese, Hindi, Spanish, German, Greek, Italian", "en")
+        speak("If you would like to review this list, you can do so on GitHub", "en")
+        speak("At any time, if you would like to return to setup, to change language or mode, say. setup. when the microphone is receiving english", "en")
+        speak("To leave the translator, say leave, or exit in english", "en")
+        speak("Tutorial complete! Beginning setup...", "en")
+        initialise()
+
+
+
+
+
+
 
 
 
 def main():
     
     initialise()
-    while in_use == True:
-        retrieve()
-        if in_use == True:
+    print("--------------LANGUAGE1 enGB-----------------")
+    print("")
+    if convo_mode == True:
+        while in_use == True:
+            retrieve('en-GB')
+            if in_use == True:
+                print("")
+                translate_to(srinp,thelang,'en')
             print("")
-            translate_to(srinp,thelang)
+            print("--------------LANGUAGE2-----------------")
+            print("")
+            if in_use == True:
+                retrieve(oppoLang)
+            if in_use == True:
+                translate_to(srinp,'en',thelang)
+            print("")
+            print("")
+            print("--------------LANGUAGE1 enGB-----------------")
+            print("")
+    elif convo_mode == False:
+        while in_use == True:
+            retrieve('en-GB')
+            if in_use == True:
+                print("")
+                translate_to(srinp,thelang,'en')
         print("")
         print("")
-        print("------------------------------------------")
+        print("-------------------------------")
         print("")
+
+
+
+
         
 main()
+#initialise()
+#retrieve('hi-IN')
+
+
 
 
